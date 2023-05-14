a9ff00b5a287b50a444b7306ee487d8da57bcc19
StelMar11 <salgadostellamaris@gmail.com>
2023-05-13 22:05:45 -0300

    skillJson

--- src/app/componentes/habilidades/habilidades.component.ts
+++ src/app/componentes/habilidades/habilidades.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatosService } from 'src/app/servicios/datos.service';
 
 @Component({
   selector: 'app-habilidades',
   templateUrl: './habilidades.component.html',
   styleUrls: ['./habilidades.component.css']
 })
-export class HabilidadesComponent {
+export class HabilidadesComponent implements OnInit{
+  habilidad: any;
+  tituloHabil: string='';
+  constructor(private datos:DatosService){}
+  ngOnInit(): void {
+    this.datos.getDatos().subscribe(data =>{
+      this.habilidad=data.habilidades;
+      this.tituloHabil=data.tituloHabil
+    })
+    
+  }
 
 }
