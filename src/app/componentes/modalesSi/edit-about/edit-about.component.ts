f0d0732b85e1a481727b9ec97760c2254cf93ba0
StelMar11 <salgadostellamaris@gmail.com>
2023-06-19 22:01:37 -0300

    formReactivoAbout

--- src/app/componentes/modalesSi/edit-about/edit-about.component.ts
+++ src/app/componentes/modalesSi/edit-about/edit-about.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-edit-about',
   templateUrl: './edit-about.component.html',
   styleUrls: ['./edit-about.component.css']
 })
-export class EditAboutComponent {
-
+export class EditAboutComponent  {
+  form: FormGroup;
+  
+  constructor(private formBuilder: FormBuilder){ 
+    this.form= this.formBuilder.group({
+       Text:['', [Validators.required]],
+      })
+   
+      
+}
 }
