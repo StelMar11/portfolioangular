55851a42e080b4671cf7d1ea6fb4f958f4d673a3
StelMar11 <salgadostellamaris@gmail.com>
2023-07-04 19:52:54 -0300

    modalHtmlCorregir

--- src/app/componentes/modalesSi/edit-educacion/edit-educacion.component.ts
+++ src/app/componentes/modalesSi/edit-educacion/edit-educacion.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-educacion',
   templateUrl: './edit-educacion.component.html',
   styleUrls: ['./edit-educacion.component.css']
 })
-export class EditEducacionComponent {
+export class EditEducacionComponent implements OnInit {
+  form: FormGroup;
+  constructor (private formBuilder: FormBuilder){
+    this.form= this.formBuilder.group({
+      text:[' ', [Validators.required]],
 
-}
+    })
+  }
+  ngOnInit(): void {}
+    get Text(){
+      return this.form.get("text");
+      }
+  onEnviar(event:Event){
+    event.preventDefault;
+    if(this.form.valid){
+      alert("Todo se envió correctamente")
+    }
+  }
+    
+    
+  }
+
+
