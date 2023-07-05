02df9b73d85317c0b005a6ee8952f2df1b3d0ae7
StelMar11 <salgadostellamaris@gmail.com>
2023-07-05 19:19:17 -0300

    mejoraModalEduc

--- src/app/componentes/modalesSi/edit-educacion/edit-educacion.component.ts
+++ src/app/componentes/modalesSi/edit-educacion/edit-educacion.component.ts
@@ -10,14 +10,26 @@ export class EditEducacionComponent implements OnInit {
   form: FormGroup;
   constructor (private formBuilder: FormBuilder){
     this.form= this.formBuilder.group({
-      text:[' ', [Validators.required]],
+      text1:[' ', [Validators.required]],
+      url1:[' ', [Validators.required]],
+      text2:[' ', [Validators.required]],
+      url2:[' ', [Validators.required]],
 
     })
   }
   ngOnInit(): void {}
-    get Text(){
-      return this.form.get("text");
+    get Text1(){
+      return this.form.get("text1");
       }
+      get Url1(){
+        return this.form.get("url1");
+        }
+        get Text2(){
+          return this.form.get("text2");
+          }
+          get Url2(){
+            return this.form.get("url2");
+            }
   onEnviar(event:Event){
     event.preventDefault;
     if(this.form.valid){
