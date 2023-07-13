fe56c50d9d9e1622f076e97fb886c39fd4983aa0
StelMar11 <salgadostellamaris@gmail.com>
2023-07-13 17:08:25 -0300

    modalBannerModificar

--- src/app/componentes/modalesSi/edit-banner/edit-banner.component.ts
+++ src/app/componentes/modalesSi/edit-banner/edit-banner.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-edit-banner',
   templateUrl: './edit-banner.component.html',
   styleUrls: ['./edit-banner.component.css']
 })
-export class EditBannerComponent {
+export class EditBannerComponent implements OnInit{
+  public form: FormGroup;
+  constructor (private formBuilder: FormBuilder){
+    this.form= this.formBuilder.group({
+      url:[' ', ],
+      text:[' ',]
+  })
+}
+  ngOnInit(): void {}
+  
+get Url(){
+      return this.form.get("url");
+      }
+      get Text(){
+        return this.form.get("text");
+        }
+  
+  onEnviar(event:Event){
+    event.preventDefault;
+    if(this.form.valid){
+      alert("Todo se envió correctamente")
+    }
+  }
 
 }
